ac8c2cb504afbe58f6dd9b6e087dae6be941719d
Leslie Lauw <lauw2498@live.nl>
2022-07-20 20:14:50 +0200

    Finished and tested with 93% code coverage

--- contracts/EvolvingTitan.sol
+++ contracts/EvolvingTitan.sol
@@ -22,31 +22,46 @@ contract EvolvingTitan is AccessControlEnumerable, ERC721Enumerable, Ownable, Si
     // Events
     event tokenMinted(uint tokenID, address minterAddress);
 
-    //Errors
-    error AddressAlreadyClaimed();
+    // Errors
+    error AddressAlreadyClaimed(address _address);
     error InvalidToken();
-    error TokenIDDoesNotExist();
-    error PermissionDenied();
+    error TokenIDDoesNotExist(uint256 tokenId);
+    error PermissionDenied(string errorMessage, address caller);
+
+    // Function modifiers
+    modifier callerIsAdmin() {
+        if(!hasRole(DEFAULT_ADMIN_ROLE,  _msgSender())) revert PermissionDenied("Caller is not an admin",  _msgSender());
+        _;
+    }
+
+    modifier callerIsSigner() {
+        if(!hasRole(SIGNER_ROLE,  _msgSender())) revert PermissionDenied("Caller is not a signer",  _msgSender());
+        _;
+    }
+
+    modifier callerIsTransferrer() {
+        if(!hasRole(TRANSFER_ROLE,  _msgSender())) revert PermissionDenied("Caller is not a transferrer",  _msgSender());
+        _;
+    }
 
     constructor() ERC721("KOIOS Evolving Titan", "eTITAN") {  
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(SIGNER_ROLE,  msg.sender);
-        _setupRole(TRANSFER_ROLE, msg.sender);      
+        _setupRole(DEFAULT_ADMIN_ROLE,  _msgSender());
+        _setupRole(SIGNER_ROLE,   _msgSender());
+        _setupRole(TRANSFER_ROLE,  _msgSender());      
     }
 
     function claim(string calldata _salt, bytes calldata _token) external {
         uint256 _supply = totalSupply();
         uint256 _tokenId = _supply + 1;
-        if(claimedNFT[msg.sender]) revert AddressAlreadyClaimed();
-        if(!verifyTokenForAddress(_salt, _token, msg.sender)) revert InvalidToken();
-        _safeMint(msg.sender, _tokenId);
-        claimedNFT[msg.sender] = true;
-        emit tokenMinted(_tokenId, msg.sender);
+        if(claimedNFT[ _msgSender()]) revert AddressAlreadyClaimed(_msgSender());
+        if(!verifyTokenForAddress(_salt, _token,  _msgSender())) revert InvalidToken();
+        _safeMint( _msgSender(), _tokenId);
+        claimedNFT[ _msgSender()] = true;
+        emit tokenMinted(_tokenId,  _msgSender());
     }
 
     // Sets the signer for the ECDSA signature
-    function setSigner(address _signer) public {
-        if(!hasRole(SIGNER_ROLE, _msgSender())) revert PermissionDenied();
+    function setSigner(address _signer) public callerIsSigner {
         _setSigner(_signer);
     }
 
@@ -57,12 +72,12 @@ contract EvolvingTitan is AccessControlEnumerable, ERC721Enumerable, Ownable, Si
         return _tokenBaseURI;
     }
 
-    function setBaseURI(string calldata _newURI) external onlyOwner {
+    function setBaseURI(string calldata _newURI) external callerIsAdmin {
         _tokenBaseURI = _newURI;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        if(!_exists(tokenId)) revert TokenIDDoesNotExist();
+        if(!_exists(tokenId)) revert TokenIDDoesNotExist(tokenId);
 
         string memory baseURI = _baseURI();
         string memory json = ".json";
@@ -75,9 +90,8 @@ contract EvolvingTitan is AccessControlEnumerable, ERC721Enumerable, Ownable, Si
     // Override _transfer function from the ERC-721 standard, making transfers only available to addresses that have a TRANSFER_ROLE
     function _transfer(address from,
         address to,
-        uint256 tokenId) internal virtual override(ERC721){
+        uint256 tokenId) internal virtual override(ERC721) callerIsTransferrer {
             super._transfer(from, to, tokenId);
-        if(!hasRole(TRANSFER_ROLE, _msgSender())) revert PermissionDenied();
     }
 
     /**
